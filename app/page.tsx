24be358b4007ea20217228d824e3dc81fc785f14
nizardgamliel-hue <nizardgamliel@gmail.com>
2026-08-18 01:08:09 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold text-nova-purple">Nova est en ligne !</h1>
+    </div>
+  );
+}
